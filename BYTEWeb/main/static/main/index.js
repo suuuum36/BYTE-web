1a6343cafde5d56104919f0414adb6923ec4ab8f
suuuum36 <lsm3191@snu.ac.kr>
2020-11-16 11:40:03 +0900

    팝업

---
+++ BYTEWeb/main/static/main/index.js
@@ -0,0 +1,12 @@
+
+$('.notice-button').click(function(){
+    $('.notice-board').css("display", "block");
+});
+
+$('.cancel-button').click(function(){
+    $('.notice-board').css("display", "none");
+});
+
+$('.notice-board').click(function(){
+    $('.notice-board').css("display", "none");
+});
