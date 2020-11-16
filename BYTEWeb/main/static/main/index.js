38e9b71fb03bbf7c7f2614020e24663958695a0f
suuuum36 <lsm3191@snu.ac.kr>
2020-11-17 01:23:40 +0900

    web version review 남음

--- BYTEWeb/main/static/main/index.js
+++ BYTEWeb/main/static/main/index.js
@@ -1,3 +1,24 @@
+/* 네비게이터 */
+
+$(".n-1").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".function-description").offset().top
+    },500);
+});
+
+$(".n-2").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".review").offset().top
+    },700);
+});
+
+$(".n-3").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".links").offset().top
+    },1000);
+});
+
+/* 유의사항 팝업 */
 
 $('.notice-button').click(function(){
     $('.notice-board').css("display", "block");
@@ -10,3 +31,4 @@ $('.cancel-button').click(function(){
 $('.notice-board').click(function(){
     $('.notice-board').css("display", "none");
 });
+
--- BYTEWeb/main/views.py
+++ BYTEWeb/main/views.py
@@ -1,18 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.urls import reverse
 from .models import Subscriber
-import requests, json
+import requests, json, re
 
 # Create your views here.
 def index (request):
     if request.method == 'GET': 
-        return render(request, 'main/index.html')
+        
+        URL =  "https://api.stibee.com/v1/lists/81111/subscribers"
+        
+        headers = {
+            "AccessToken" : "ed00b5b09dc81a0ee29ae26577bed3c2d5f38e5caedd824fe905ff5c6d9cbd4d6c8a79f45cb4641f9d54f4de85e713c83d6e50524343bd1fd4049e91d064175d",
+            "listID" : "81111",
+            "Content-Type": "application/json"
+        }
+        
+        response_get = requests.get(URL, headers = headers)
+        user_data = response_get.json()
+        value = user_data['Value']
+        emails = [email['email'] for email in value]
+        user_num = len(emails)
+        
+        return render(request, 'main/index.html', {'user_num' : user_num})
     
     elif request.method == 'POST':
+        
+        URL =  "https://api.stibee.com/v1/lists/81111/subscribers"
+        
+        headers = {
+            "AccessToken" : "ed00b5b09dc81a0ee29ae26577bed3c2d5f38e5caedd824fe905ff5c6d9cbd4d6c8a79f45cb4641f9d54f4de85e713c83d6e50524343bd1fd4049e91d064175d",
+            "listID" : "81111",
+            "Content-Type": "application/json"
+        }
         
         name = request.POST['name']
         email = request.POST['email']
         
-        URL =  "https://api.stibee.com/v1/lists/81111/subscribers"
+        
         data = {
         "eventOccuredBy": "MANUAL",
         "confirmEmailYN": "Y",
@@ -25,24 +49,37 @@ def index (request):
             }
         ]
         }
-
-        headers = {
-            "AccessToken" : "ed00b5b09dc81a0ee29ae26577bed3c2d5f38e5caedd824fe905ff5c6d9cbd4d6c8a79f45cb4641f9d54f4de85e713c83d6e50524343bd1fd4049e91d064175d",
-            "listID" : "81111",
-            "Content-Type": "application/json"
-        }
         
-
         response_get = requests.get(URL, headers = headers)
         user_data = response_get.json()
         value = user_data['Value']
         emails = [email['email'] for email in value]
-
-        if email in emails:
-            print('already exists')
+        user_num = len(emails)
+        
+        if name != '' and email != '':
             
+            email_match = re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
+            
+            if email_match != None:
+                
+                if email in emails:
+                    exist_alarm = "이미 존재하는 이메일입니다."
+                    print('already exists')
+                    
+                else:
+                    checked = request.POST.getlist('checkbox')
+                    if checked != [] :
+                        response_post = requests.post(URL, data=json.dumps(data), headers = headers)
+                        exist_alarm = "메일함에서 인증을 완료해주세요!"
+                        print(response_post.text)
+                        print(checked)
+                    else:
+                        exist_alarm = "이용약관에 동의해주세요."
+                        
+            else:
+                exist_alarm = "이메일 형식을 맞춰주세요."
+                
         else:
-            response_post = requests.post(URL, data=json.dumps(data), headers = headers)
-            print(response_post.text)
+            exist_alarm = "이름과 이메일을 모두 작성해주세요."
             
-        return redirect('index')
+        return render(request, 'main/index.html', {'user_num' : user_num, 'exist_alarm' : exist_alarm , 'name':name, 'email':email})  
