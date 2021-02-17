b9815a9cd22298cf4eb1c067d0eb8838a771e212
suuuum36 <lsm3191@snu.ac.kr>
2021-02-17 16:13:53 +0900

    DB 수정

--- BYTEWeb/BYTEWeb/settings.py
+++ BYTEWeb/BYTEWeb/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': 'mydatabase',
+    #     'NAME': 'db.sqlite3',
     # }
 }
 
---
+++ BYTEWeb/main/static/main/popup.js
@@ -0,0 +1,15 @@
+
+const popupButton = document.querySelector('.popup_close');
+const popupBody = document.querySelector('.popup')
+
+/*일반 닫기*/
+function hidePopup () {
+    popupBody.style.display = 'none';
+}
+
+// popupButton.addEventListener('click', hidePopup);
+
+// if (popupBody.style.display !== 'none') {
+//     document.body.addEventListener('click', hidePopup);
+// }
+
